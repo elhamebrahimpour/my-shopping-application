6d39d10020ff05b94af2c3376501eb3acbb6937c
Elham <ebrahimpourelham44@gmail.com>
2021-12-01 12:33:27 +0330

    My_First_Application

--- app/src/main/java/com/elham/shoppingproject/views/LoginFragment.kt
+++ app/src/main/java/com/elham/shoppingproject/views/LoginFragment.kt
@@ -17,7 +17,7 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding= FragmentLoginBinding.inflate(inflater,container,false)
         return binding.root
